7a789921a9068e94fd3c58e58c2c616248705698
alddl <131025_@naver.com>
2022-07-26 20:35:26 +0900

    2022-07-25 안드로이드 스튜디오 예약

--- app/src/main/java/com/example/myapplication/clock.java
+++ app/src/main/java/com/example/myapplication/clock.java
@@ -1,2 +1,45 @@
-package com.example.myapplication;public class clock {
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class clock extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clock);
+        TimePicker tp = findViewById(R.id.tp);
+        Button tpbtn = findViewById(R.id.timebtn);
+        TextView tptv = findViewById(R.id.timetv);
+
+        tp.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                Integer hour = tp.getHour();
+                Integer min = tp.getMinute();
+                tptv.setText(hour + "시 " + min +"분");
+            }
+        });
+
+        tpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(),MainActivity.class);
+                Integer hour = tp.getHour();
+                Integer min = tp.getMinute();
+                tptv.setText(hour + "시 " + min +"분 예약");
+                it.putExtra("time",hour + "시 " + min +"분");
+                setResult(100, it);
+                finish();
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/myapplication/date.java
+++ app/src/main/java/com/example/myapplication/date.java
@@ -1,2 +1,47 @@
-package com.example.myapplication;public class date {
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+public class date extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.date);
+        DatePicker dp = findViewById(R.id.dp);
+        Button dpbtn = findViewById(R.id.dpbtn);
+        TextView dptv = findViewById(R.id.dptv);
+
+        dp.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+                Integer month = dp.getMonth();
+                Integer day = dp.getDayOfMonth();
+                dptv.setText(month + "월 " + day +"일");
+            }
+        });
+
+        dpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer month = dp.getMonth();
+                Integer day = dp.getDayOfMonth();
+                dptv.setText(month + "월 " + day +"일 예약");
+                Intent it = new Intent(getApplicationContext(),MainActivity.class);
+                it.putExtra("date",month + "월 " + day +"일");
+                setResult(101, it);
+                finish();
+            }
+        });
+    }
 }
